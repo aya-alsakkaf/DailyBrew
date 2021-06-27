9fd25020407b016b61d64a176d1abb94ef983332
Bashayer-aleid <71448795+Bashayer-aleid@users.noreply.github.com>
2021-06-27 20:15:28 +0300

    add mobile number validation

--- src/signupVal.js
+++ src/signupVal.js
@@ -5,16 +5,20 @@ function formVal(){
     var upass = document.getElementById("upass");
     var repass = document.forms.signUpForm.repass.value;
     var urepass = document.getElementById("urepass");
+    var tel = document.forms.signUpForm.tel.value;
+    var utel = document.getElementById("utel");
 
     if(emailVal(email, uemail)){
-     if(passVal(password, upass)){
+        if(passVal(password, upass)){
            if(confirmPass(repass, urepass, password)){
-               return true;
-           }
+                if(telVal(tel, utel)){
+                    return true;
+                }
+            }
         }
     }
     
-    return false;
+     return false;
 }
 
 function emailVal(email, uemail){
@@ -46,8 +50,23 @@ function confirmPass(repass, urepass, password){
         urepass.innerHTML = "<p style=\"color:red; font-size=14px;\"> Passwords don't match </p>";
         return false;
     }
-    else {
+    else if(repass == password) {
         urepass.innerHTML = "";
+        return true;
+    } else {
+        urepass.innerHTML = "";
+        return true;
+    }
+}
+
+function telVal(tel, utel){
+    var reg = /^(\(\d{3}\))?[-. ]?(\d{4})[-. ](\d{4})$/;
+    if(!reg.test(tel)){
+        utel.innerHTML = "<p style=\"color:red; font-size=14px;\"> Invalid Phone number</p>";
+        return false;
+    }
+    else {
+        utel.innerHTML = "";
         return true;
     }
 }
